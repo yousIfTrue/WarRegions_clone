5d87e710e2d4562eafc2ab47cf31005f46794044
yousIfTrue <For.Try.3.x@gmail.com>
2025-11-25 00:27:41 +0200

    📦 اصلاح TerrainEffect.cs | 🕒 2025-11-25 00:27:41

--- Core/Models/Terrain/TerrainEffect.cs
+++ Core/Models/Terrain/TerrainEffect.cs
@@ -1,5 +1,22 @@
 using System;
-namespace: WarRegions.Core.Models.Terrain
+namespace WarRegions.Core.Models.Terrain
 {
-    
+    public class TerrainEffect
+    {
+        public string EffectName { get; set; }
+        public int MovementModifier { get; set; }
+        public int DefenseModifier { get; set; }
+        public int AttackModifier { get; set; }
+        
+        public TerrainEffect()
+        {
+            EffectName = "Default";
+            MovementModifier = 0;
+            DefenseModifier = 0;
+            AttackModifier = 0;
+        }
+    }
 }
+
+
+
