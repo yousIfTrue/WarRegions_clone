0da9ab607b47dfb89b66ce1ef2179ad9ca993c10
yousIfTrue <For.Try.3.x@gmail.com>
2025-11-25 07:04:58 +0200

    📦 ازالة namespaces الخاطئة | 🕒 2025-11-25 07:04:58

--- Core/GlobalUsings.cs
+++ Core/GlobalUsings.cs
@@ -1,35 +1,27 @@
-// Core/GlobalUsings.cs
 global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 
-// Core Models - جميع الـ namespaces المحتملة
+// Core Models - بناءً على الهيكل الفعلي
 global using WarRegions.Core.Models;
 global using WarRegions.Core.Models.Terrain;
 global using WarRegions.Core.Models.Units;
 global using WarRegions.Core.Models.Level;
 global using WarRegions.Core.Models.Economy;
-global using WarRegions.Models;
-global using WarRegions.Models.Terrain;
-global using WarRegions.Models.Units;
-global using WarRegions.Models.Level;
-global using WarRegions.Models.Economy;
+global using WarRegions.Core.Models.Development;
 
-// Interfaces - جميع الـ namespaces المحتملة  
+// Interfaces - بناءً على الهيكل الفعلي
 global using WarRegions.Core.Interfaces;
-global using WarRegions.Interfaces;
 
-// Controllers - جميع الـ namespaces المحتملة
+// Controllers - بناءً على الهيكل الفعلي
 global using WarRegions.Core.Controllers;
 global using WarRegions.Core.Controllers.Pathfinding;
 global using WarRegions.Core.Controllers.Economy;
-global using WarRegions.Controllers;
-global using WarRegions.Controllers.Pathfinding;
-global using WarRegions.Controllers.Economy;
 
-// أنواع محددة للراحة
-global using MovementType = WarRegions.Core.Models.Units.MovementType;
+// أنواع شائعة الاستخدام
 global using TerrainTile = WarRegions.Core.Models.Terrain.TerrainTile;
+global using MovementType = WarRegions.Core.Models.Units.MovementType;
 global using UnitCard = WarRegions.Core.Models.Units.UnitCard;
 global using UnitRarity = WarRegions.Core.Models.Units.UnitRarity;
+global using Player = WarRegions.Core.Models.Player;
