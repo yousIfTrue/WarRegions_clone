adbebbfa91daf6d81b838c025f5697cbdffc491c
yousIfTrue <For.Try.3.x@gmail.com>
2025-11-26 17:02:25 +0200

    📦  | 🕒 2025-11-26 17:02:25

---
+++ Presentation/GlobalUsings.cs
@@ -0,0 +1,13 @@
+// استخدامات Core (التي يعتمد عليها)
+global using WarRegions.Core;
+global using WarRegions.Core.Models;
+global using WarRegions.Core.Interfaces;
+global using WarRegions.Core.Engine;
+
+// استخدامات Presentation الداخلية
+global using WarRegions.Presentation.Interface2D.Scripts;
+global using WarRegions.Presentation.Interface3D.Scripts;
+
+// استخدامات النظام
+global using System;
+global using System.Collections.Generic;
