ed11cf9185a96ac1e7b42521f85084173a02a5c9
yousIfTrue <For.Try.3.x@gmail.com>
2025-11-26 19:39:10 +0200

    📦 more fixes😮‍💨 | 🕒 2025-11-26 19:39:10

--- Core/GlobalUsings.cs
+++ Core/GlobalUsings.cs
@@ -25,15 +25,15 @@ global using WarRegions.Core.Models.Units;
 // global using WarRegions.CLI;
 
 // أنواع أساسية للراحة
-global using Army = WarRegions.Core.Models.Army;
-global using Player = WarRegions.Core.Models.Player;
-global using Region = WarRegions.Core.Models.Region;
-global using GameState = WarRegions.Core.Models.GameState;
-global using TerrainTile = WarRegions.Core.Models.Terrain.TerrainTile;
+global using Army         = WarRegions.Core.Models.Army;
+global using Player       = WarRegions.Core.Models.Player;
+global using Region       = WarRegions.Core.Models.Region;
+global using GameState    = WarRegions.Core.Models.GameState;
+global using TerrainTile  = WarRegions.Core.Models.Terrain.TerrainTile;
 global using MovementType = WarRegions.Core.Models.Units.MovementType;
-global using UnitCard = WarRegions.Core.Models.Units.UnitCard;
-global using UnitRarity = WarRegions.Core.Models.Units.UnitRarity;
-global using ShopItem = WarRegions.Core.Models.Economy.ShopItem;
-global using Transaction = WarRegions.Core.Models.Economy.Transaction;
-global using SpawnPoint = WarRegions.Core.Models.Level.SpawnPoint;
-global using DevConfig = WarRegions.Core.Models.Development.DevConfig;
+global using UnitCard     = WarRegions.Core.Models.Units.UnitCard;
+global using UnitRarity   = WarRegions.Core.Models.Units.UnitRarity;
+global using ShopItem     = WarRegions.Core.Models.Economy.ShopItem;
+global using Transaction  = WarRegions.Core.Models.Economy.Transaction;
+global using SpawnPoint   = WarRegions.Core.Models.Level.SpawnPoint;
+global using DevConfig    = WarRegions.Core.Models.Development.DevConfig;
