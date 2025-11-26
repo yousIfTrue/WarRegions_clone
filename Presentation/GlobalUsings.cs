089ef9533ba58c4bace2e7c418ace38e79c0b12d
yousIfTrue <For.Try.3.x@gmail.com>
2025-11-26 20:07:23 +0200

    📦 فصل بناء كل مشروع لوحده | 🕒 2025-11-26 20:07:23

--- Presentation/GlobalUsings.cs
+++ Presentation/GlobalUsings.cs
@@ -1,13 +1,12 @@
-// استخدامات Core (التي يعتمد عليها)
+
 global using WarRegions.Core;
 global using WarRegions.Core.Models;
 global using WarRegions.Core.Interfaces;
 global using WarRegions.Core.Engine;
-
-// استخدامات Presentation الداخلية
 global using WarRegions.Presentation.Interface2D.Scripts;
 global using WarRegions.Presentation.Interface3D.Scripts;
-
-// استخدامات النظام
 global using System;
 global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
